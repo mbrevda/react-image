8018559d1a950f56922e6406f6a591ed6b0b31ab
Moshe Brevda <mbrevda@gmail.com>
2016-10-28 14:52:49 +0300

    Working test env

--- src/index.js
+++ src/index.js
@@ -3,51 +3,55 @@ import React, {Component} from 'react'
 const { node, oneOfType, string, array } = React.PropTypes
 
 class Img extends Component {
-    static propTypes = {
-        loader: node,
-        unloader: node,
-        src: oneOfType([string, array])
+  static propTypes = {
+    loader: node,
+    unloader: node,
+    src: oneOfType([string, array])
+  }
+
+  static defaultProps = {
+    loader: false,
+    src: []
+  }
+
+  state = {currentImageIndex: 0, isLoading: true, isLoaded: false}
+
+  onLoad = () => this.setState({isLoaded: true})
+
+  onError = () => {
+    // overshoot by one. If no image is found, this will prevent a "broken"
+    // placeholder from showing
+    if (this.state.currentImageIndex + 1 < this.state.imageList.length) {
+      this.setState({currentImageIndex: ++this.state.currentImageIndex})
     }
+  }
 
-    static defaultProps = {loader: false}
-
-    onLoad = () => this.setState({isLoaded: true})
-
-    onError = () => {
-        // overshoot by one. If no image is found, this will prevent a "broken"
-        // placeholder from showing
-        if (this.state.currentImageIndex + 1< this.state.imageList.length) {
-            this.setState({currentImageIndex: ++this.state.currentImageIndex})
-        }
+  placeholder = () => {
+    if (!this.state.isLoaded) {
+      this.state.isLoading ? this.props.loader : this.props.unloader
     }
+  }
 
-    placeholder = () => {
-        if(!this.state.isLoaded) {
-            this.state.isLoading ? this.props.loader : this.props.unloader
-        }
-    }
+  componentWillMount () {
+    this.setState({imageList: typeof this.props.src === 'string' ? [this.props.src] : this.props.src})
+  }
 
-    state = {currentImageIndex: 0, isLoading: true, isLoaded: false}
+  render () {
+    let {src, loader, unloader, ...rest} = this.props //eslint-disable-line
+    src = this.state.imageList[this.state.currentImageIndex] || ''
 
-    componentWillMount() {
-        this.setState({imageList: typeof this.props.src == 'string' ? [this.props.src] : this.props.src})
-    }
-
-    render() {
-        const {src, loader, unloader, ...rest} = this.props //eslint-disable-line
-
-        return (
-            <span>
-                <img
-                    src={this.state.imageList[this.state.currentImageIndex]}
-                    onError={this.onError}
-                    onLoad={this.onLoad}
-                    {...rest}
-                />
-                {this.placeholder()}
-            </span>
-        )
-    }
+    return (
+      <span>
+        <img
+          src={src}
+          onError={this.onError}
+          onLoad={this.onLoad}
+          {...rest}
+        />
+        {this.placeholder()}
+      </span>
+    )
+  }
 }
 
 export default Img
--- src/index.test.js
+++ src/index.test.js
@@ -2,6 +2,10 @@ import React from 'react'
 import Img from './index.js'
 import renderer from 'react-test-renderer'
 
-test('say hi!', () => {
-  
+it('say hi!', () => {
+  const component = renderer.create(
+    <Img />
+  )
+  let tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
 })
