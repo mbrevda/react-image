1f5f8da74c60b9b39d1988d15096130c2b442459
Moshe Brevda <mbrevda@gmail.com>
2017-10-29 20:12:49 +0200

    Fix linting errors

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Enzyme,  {shallow} from 'enzyme';
+import Enzyme, {shallow} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import Img from './index.js'
 
