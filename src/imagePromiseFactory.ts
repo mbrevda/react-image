21e6e8215fa66818d61ed0768f2caac0d00e5824
Moshe Brevda <mbrevda@gmail.com>
2021-10-03 19:46:03 +0300

    update code syntax

--- src/imagePromiseFactory.ts
+++ src/imagePromiseFactory.ts
@@ -1,12 +1,13 @@
 // returns a Promisized version of Image() api
-export default ({decode = true, crossOrigin = ''}) => (src): Promise<void> => {
-  return new Promise((resolve, reject) => {
-    const i = new Image()
-    if (crossOrigin) i.crossOrigin = crossOrigin
-    i.onload = () => {
-      decode && i.decode ? i.decode().then(resolve).catch(reject) : resolve()
-    }
-    i.onerror = reject
-    i.src = src
-  })
-}
+export default ({decode = true, crossOrigin = ''}) =>
+  (src): Promise<void> => {
+    return new Promise((resolve, reject) => {
+      const i = new Image()
+      if (crossOrigin) i.crossOrigin = crossOrigin
+      i.onload = () => {
+        decode && i.decode ? i.decode().then(resolve).catch(reject) : resolve()
+      }
+      i.onerror = reject
+      i.src = src
+    })
+  }
