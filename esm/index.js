fe4d6ddc35ad30e7af3f51c1a93da23797a2efc1
Carlos Vieira <codebycarlos@outlook.com>
2022-10-17 20:15:55 +0100

    remove build files

--- esm/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Img } from './Img.js';
-export { u as useImage } from './useImage-37bfa40c.js';
-import '@babel/runtime/helpers/defineProperty';
-import '@babel/runtime/helpers/objectWithoutPropertiesLoose';
-import 'react';
-//# sourceMappingURL=index.js.map
--- esm/useImage.js
+++
@@ -1,4 +0,0 @@
-import '@babel/runtime/helpers/defineProperty';
-import 'react';
-export { u as default } from './useImage-37bfa40c.js';
-//# sourceMappingURL=useImage.js.map
