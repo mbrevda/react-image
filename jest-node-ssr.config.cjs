3ddc50285332de591e760b29866644bc7c310315
Moshe Brevda <mbrevda@gmail.com>
2023-03-10 11:43:32 +0200

    update deps

--- jest-node-ssr.config.cjs
+++ jest-node-ssr.config.cjs
@@ -2,7 +2,6 @@ module.exports = {
   testMatch: ['**/src/**/*.ssr-test.js'],
   testURL: 'http://localhost',
   testEnvironment: 'node',
-  preset: 'ts-jest/presets/js-with-babel',
   globals: {
     'ts-jest': {
       babelConfig: true,
--- jest.config.cjs
+++ jest.config.cjs
@@ -2,12 +2,9 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts*'],
   coverageReporters: ['json', 'lcov', 'text-summary', 'html'],
-  preset: 'ts-jest/presets/js-with-babel',
   testURL: 'http://localhost',
   testEnvironment: 'jsdom',
-  globals: {
-    'ts-jest': {
-      babelConfig: true,
-    },
+  transform: {
+    '^.+\\.tsx?$': 'esbuild-jest',
   },
 }
